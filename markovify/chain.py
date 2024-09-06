9a4480f52589aa6706a3b1d43394e73f2854d655
tropicbliss <tropicbliss@protonmail.com>
2024-09-06 11:44:41 +0800

    formatting

--- markovify/chain.py
+++ markovify/chain.py
@@ -6,14 +6,18 @@ import sqlite3
 import os
 from markovify.text import word_join
 
+
 class DatabaseError(Exception):
     pass
+
 
 class ParamError(Exception):
     pass
 
+
 BEGIN = "___BEGIN__"
 END = "___END__"
+
 
 def accumulate(iterable, func=operator.add):
     it = iter(iterable)
@@ -23,10 +27,12 @@ def accumulate(iterable, func=operator.add):
         total = func(total, element)
         yield total
 
+
 def compile_next(next_dict):
     words = list(next_dict.keys())
     cff = list(accumulate(next_dict.values()))
     return [words, cff]
+
 
 class Chain:
     def __init__(self, state_size, path):
@@ -40,14 +46,14 @@ class Chain:
         for run in corpus:
             items = ([BEGIN] * self.state_size) + run + [END]
             for i in range(len(run) + 1):
-                state = tuple(items[i : i + self.state_size])
+                state = tuple(items[i: i + self.state_size])
                 follow = items[i + self.state_size]
                 if state not in model:
                     model[state] = {}
                 if follow not in model[state]:
                     model[state][follow] = 0
                 model[state][follow] += 1
-        
+
         try:
             os.remove(self.path)
         except FileNotFoundError:
@@ -67,14 +73,16 @@ class Chain:
             value INTEGER
             );
         ''')
-        cursor.execute("INSERT INTO metadata (key, value) VALUES (?, ?)", ("state_size", self.state_size))
+        cursor.execute("INSERT INTO metadata (key, value) VALUES (?, ?)",
+                       ("state_size", self.state_size))
         for (state, next_dict) in model.items():
             key = state
             value = compile_next(next_dict)
             raw_key = word_join(key)
             raw_words = json.dumps(value[0])
             raw_cum_freq = json.dumps(value[1])
-            cursor.execute("INSERT INTO metadata (key, words, cum_freq) VALUES (?, ?, ?)", (raw_key, raw_words, raw_cum_freq))
+            cursor.execute("INSERT INTO metadata (key, words, cum_freq) VALUES (?, ?, ?)",
+                           (raw_key, raw_words, raw_cum_freq))
         conn.commit()
         conn.close()
 
@@ -102,7 +110,8 @@ class Chain:
     def from_db(cls, path):
         conn = sqlite3.connect(path)
         cursor = conn.cursor()
-        cursor.execute("SELECT value FROM metadata WHERE key = ?", ("state_size",))
+        cursor.execute(
+            "SELECT value FROM metadata WHERE key = ?", ("state_size",))
         row = cursor.fetchone()
         if row:
             state_size = row[0]
@@ -110,15 +119,17 @@ class Chain:
             raise DatabaseError("Invalid database file")
         return cls(state_size, path=path)
 
+
 def index_into_state(state, cursor):
-        raw_state = word_join(state)
-        cursor.execute("SELECT words, cum_freq FROM data WHERE key = ?", (raw_state,))
-        row = cursor.fetchone()
-        if row:
-            raw_words = row[0]
-            raw_cum_freq = row[1]
-        else:
-            raise KeyError
-        words = json.loads(raw_words)
-        cum_freq = json.loads(raw_cum_freq)
-        return words, cum_freq
+    raw_state = word_join(state)
+    cursor.execute(
+        "SELECT words, cum_freq FROM data WHERE key = ?", (raw_state,))
+    row = cursor.fetchone()
+    if row:
+        raw_words = row[0]
+        raw_cum_freq = row[1]
+    else:
+        raise KeyError
+    words = json.loads(raw_words)
+    cum_freq = json.loads(raw_cum_freq)
+    return words, cum_freq
--- markovify/text.py
+++ markovify/text.py
@@ -1,13 +1,15 @@
 import re
 from .chain import Chain
 
+
 class Output:
     def __init__(self, path):
         self.chain = Chain.from_db(path)
-    
+
     def make_sentence(self):
         words = self.chain.walk()
         return word_join(words)
+
 
 class Input:
     def __init__(self, text, state_size, path):
@@ -17,7 +19,7 @@ class Input:
 
     def sentence_split(self, text):
         return re.split(r"\s*\n\s*", text)
-    
+
     def generate_corpus(self, text):
         sentences = self.sentence_split(text)
         runs = map(word_split, sentences)
@@ -26,8 +28,10 @@ class Input:
 
 word_split_pattern = re.compile(r"\s+")
 
+
 def word_split(sentence):
     return re.split(word_split_pattern, sentence)
+
 
 def word_join(words):
     return " ".join(words)
