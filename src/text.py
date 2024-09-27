9c5d8a30371390b9a3aab73c63effdc0169f9934
tropicbliss <tropicbliss@protonmail.com>
2024-09-27 16:09:53 +0800

    E

--- src/text.py
+++ src/text.py
@@ -1,17 +1,17 @@
 from .chain import Chain
-from .text_stitcher import WordSticher
+from .util import word_join
 
 
 class Output:
-    def __init__(self, path, stitcher=WordSticher()):
-        self.chain = Chain.from_db(path, stitcher)
+    def __init__(self, path):
+        self.chain = Chain.from_db(path)
 
     def make_sentence(self):
         words = self.chain.walk()
-        return self.chain.word_stitcher.word_join(words)
+        return word_join(words)
 
 
 class Input:
-    def __init__(self, input_path, output_path, state_size, append=False, stitcher=WordSticher()):
-        self.chain = Chain(state_size, output_path, stitcher)
+    def __init__(self, input_path, output_path, state_size, append=False):
+        self.chain = Chain(state_size, output_path)
         self.chain.build(path=input_path, append=append)
--- src/text_stitcher.py
+++
@@ -1,9 +0,0 @@
-from .util import word_join as wjoin, word_split as wsplit
-
-
-class WordSticher:
-    def word_split(self, sentence):
-        return wsplit(sentence)
-
-    def word_join(self, words):
-        return wjoin(words)
